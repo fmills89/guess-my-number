8244626c2c9ba59e4e1188f884e8890ef6d18af5
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-05-26 17:02:10 -0500

    added styling and colors constants

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,28 @@
-import { Text } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+
+import Title from '../components/Title';
 
 function GameScreen() {
     return (
-        <Text>Game Screen!</Text>
+        <View style={styles.screen}>
+            <Title>Opponent's Guess</Title>
+        {/* GUESS */}
+        <View>
+            <Text>Higher or lower?</Text>
+            {/* + - */}
+        </View>
+        {/* <View>LOG ROUNDS</View> */}
+        </View>
     )
 };
 
 export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 24,
+
+    },
+   
+})
