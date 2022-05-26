27d4ca96431c714e97c572cd200b2f8e11eb9105
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-05-26 16:23:34 -0500

    added addl functionality and route to game screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { TextInput, View, StyleSheet, Alert } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
 
-function StartGameScreen() {
+function StartGameScreen({onPickNumber}) {
     const [enteredNumber, setEnteredNumber] = useState('');
 
     function numberInputHandler(enteredText) {
@@ -18,12 +18,14 @@ function StartGameScreen() {
         const chosenNumber = parseInt(enteredNumber);
 
         if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
-            Alert.alert('Invalid number!', 'Number has to be a number between 1 and 99.',
-            [{ text: 'Okay', style:'destructive', onPress: resetInputHandler }]
+            Alert.alert(
+                'Invalid number!', 
+                'Number has to be a number between 1 and 99.',
+                [{ text: 'Okay', style:'destructive', onPress: resetInputHandler }]
             );
             return;
         }
-        console.log('Vallid number!');
+        onPickNumber(chosenNumber);
     }
 
     return (
