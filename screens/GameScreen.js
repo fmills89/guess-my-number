752eee8451d16452d6bb6eebacae64e2e8f6af7d
FoboIX <89666151+fmills89@users.noreply.github.com>
2022-06-01 17:00:56 -0500

    created reusable components

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,11 @@
-import { useState } from 'react';
-import { Text, View, StyleSheet, Alert } from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, StyleSheet, Alert } from 'react-native';
 import NumberContainer from '../components/game/NumberContainer';
 import PrimaryButton from '../components/ui/PrimaryButton';
+import Card from '../components/ui/Card';
 
 import Title from '../components/ui/Title';
+import InstructionText from '../components/ui/InstructionText';
 
 function generateRandomBetween(min, max, exclude) {
     const rndNum = Math.floor(Math.random() * (max - min)) + min;
@@ -18,9 +20,15 @@ function generateRandomBetween(min, max, exclude) {
   let minBoundary = 1;
   let maxBoundary = 100;
 
-function GameScreen({userNumber}) {
-    const initialGuess = generateRandomBetween(minBoundary, maxBoundary, userNumber)
+function GameScreen({userNumber, onGameOver}) {
+    const initialGuess = generateRandomBetween(1, 100, userNumber)
     const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
+    useEffect(() => {
+        if (currentGuess === userNumber) {
+            onGameOver();
+        }
+    }, [currentGuess, userNumber, onGameOver]);
 
     function nextGuessHandler(direction) {
         // check 
@@ -47,13 +55,13 @@ function GameScreen({userNumber}) {
         <View style={styles.screen}>
             <Title>Opponent's Guess</Title>
             <NumberContainer>{currentGuess}</NumberContainer>
-        <View>
-            <Text>Higher or lower?</Text>
+        <Card>
+            <InstructionText>Higher or lower?</InstructionText>
             <View>
                 <PrimaryButton onPress={nextGuessHandler.bind(this, 'lower')}>-</PrimaryButton>
                 <PrimaryButton onPress={nextGuessHandler.bind(this, 'greater')}>+</PrimaryButton>
             </View>
-        </View>
+        </Card>
         {/* <View>LOG ROUNDS</View> */}
         </View>
     )
